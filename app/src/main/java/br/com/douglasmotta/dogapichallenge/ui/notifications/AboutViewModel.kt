1bec2eaf142f6a800b7b4a99a4cd0094f6d3b697
Douglas Pessoal <me@MBP-de-DOUGLAS.lan>
2022-02-02 20:20:24 +0000

    Small adjustment on about feature (blank)

--- app/src/main/java/br/com/douglasmotta/dogapichallenge/ui/notifications/AboutViewModel.kt
+++ app/src/main/java/br/com/douglasmotta/dogapichallenge/ui/notifications/AboutViewModel.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is about Fragment"
     }
     val text: LiveData<String> = _text
 }
