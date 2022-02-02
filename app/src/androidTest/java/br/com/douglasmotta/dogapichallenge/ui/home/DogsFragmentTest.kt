4cb775970a1e79e4b58e44ce0ae5b245888a014b
Douglas Pessoal <me@MBP-de-DOUGLAS.lan>
2022-02-02 20:26:18 +0000

    test(ui-testing): Set up UI tests with Hilt

---
+++ app/src/androidTest/java/br/com/douglasmotta/dogapichallenge/ui/home/DogsFragmentTest.kt
@@ -0,0 +1,5 @@
+package br.com.douglasmotta.dogapichallenge.ui.home
+
+import org.junit.Assert.*
+
+class DogsFragmentTest
---
+++ app/src/debug/java/br/com/douglasmotta/dogapichallenge/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package br.com.douglasmotta.dogapichallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
