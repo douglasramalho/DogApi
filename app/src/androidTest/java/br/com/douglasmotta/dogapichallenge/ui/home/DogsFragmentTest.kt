d98edb519e8c1e0ef3c4b830462a3d1eed5d3f45
Douglas Pessoal <me@MBP-de-DOUGLAS.lan>
2022-02-02 20:28:17 +0000

    test(ui-testing): Replace base url module in the UI tests

--- app/src/androidTest/java/br/com/douglasmotta/dogapichallenge/ui/home/DogsFragmentTest.kt
+++ app/src/androidTest/java/br/com/douglasmotta/dogapichallenge/ui/home/DogsFragmentTest.kt
@@ -1,5 +1,12 @@
 package br.com.douglasmotta.dogapichallenge.ui.home
 
-import org.junit.Assert.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import br.com.douglasmotta.dogapichallenge.framework.di.BaseUrlModule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
+@UninstallModules(BaseUrlModule::class)
+@HiltAndroidTest
 class DogsFragmentTest
